f6ec9774c259c6358ff9f2c8ebabf46c06cbe78f
atomoton <andrew@brereton.me>
2017-10-14 18:54:19 -0400

    changes to the collection module, less time wasted overall I guess

--- get_data_for_training_from_Tinder.py
+++ get_data_for_training_from_Tinder.py
@@ -14,37 +14,23 @@ print("Session started..")
 
 while True:
 
-    users = session.nearby_users()
+    try:
+        users = session.nearby_users()
+
+    except Exception as e:
+        print(e)
+        continue
 
     for user in users:
 
         photos = user.get_photos()
+        bio = user.bio
+        age = user.age
+        gender = user.gender
+
         print("Fetched user photos..")
 
         try:
-
-            ans = ""
-
-            for photo in photos:
-
-                image = imread(photo)
-                plt.imshow(image)
-                plt.pause(0.001)
-
-                input_string = "Write '.' to like, hit return to dislike, '...' to skip:\n\n"
-                ans = input(input_string).lower()
-
-                if ans == ".":
-                    save_image(image, photo, True)
-                    save_age(user.age, True)
-                elif ans == "...":
-                    break
-                else:
-                    save_image(image, photo, False)
-                    save_age(user.age, False)
-
-            if ans == "...":
-                break
 
             image = imread("null.jpg")
             plt.imshow(image)
@@ -58,11 +44,73 @@ while True:
 
                 if ans == "...":
                     break
+                elif ans == "l":
+                    try:
+                        user.like()
+                    except:
+                        pass
+                    save_bio(bio, True)
+                elif ans == "d":
+                    try:
+                        user.dislike()
+                    except:
+                        pass
+                    save_bio(bio, False)
                 elif ans == ".":
-                    save_bio(user.bio, True)
+                    save_bio(bio, True)
                 else:
-                    save_bio(user.bio, False)
+                    save_bio(bio, False)
 
+            for photo in photos:
+
+                image = imread(photo)
+                plt.imshow(image)
+                plt.pause(0.001)
+
+                input_string = "Write '.' to like, hit return to dislike, '...' to skip:\n\n"
+                ans = input(input_string).lower()
+
+                if ans == ".":
+                    save_image(image, photo, True)
+                    save_age(user.age, True)
+
+                elif ans == "l":
+                    try:
+                        user.like()
+                    except:
+                        pass
+
+                    save_image(image, photo, True)
+                    save_age(user.age, True)
+
+                elif ans == "d":
+                    try:
+                        user.dislike()
+                    except:
+                        pass
+
+                    save_image(image, photo, False)
+                    save_age(user.age, False)
+
+                elif ans == "...":
+                    break
+                else:
+                    save_image(image, photo, False)
+                    save_age(user.age, False)
+
+            ans = input("Hit enter for next user, 'l' to like, or 'd' to dislike:\n")
+
+            if ans == "l":
+                try:
+                    user.like()
+                except:
+                    pass
+
+            elif ans == "d":
+                try:
+                    user.dislike()
+                except:
+                    pass
 
         except Exception as e:
 
